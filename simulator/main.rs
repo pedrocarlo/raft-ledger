be2517eee936fa440b4ecf9fd38dce436c5412d8
pedrocarlo <pedro.muniz.carlo@gmail.com>
2025-09-07 01:31:36 -0300

    configure runtime for simulator

--- simulator/main.rs
+++ simulator/main.rs
@@ -1,1 +1,15 @@
-fn main() {}
+use rand::SeedableRng;
+use rand_chacha::ChaCha8Rng;
+
+fn main() {
+    let seed = getrandom::u64().unwrap();
+    let rng = ChaCha8Rng::seed_from_u64(seed);
+
+    let tokio_seed = tokio::runtime::RngSeed::from_bytes(&seed.to_be_bytes());
+    let rt = tokio::runtime::Builder::new_current_thread()
+        .enable_time()
+        .start_paused(true)
+        .rng_seed(tokio_seed)
+        .build_local(Default::default())
+        .unwrap();
+}
